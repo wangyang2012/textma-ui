6f3b4fc029d8c96821e8eb32b02289dc69e6b203
wangyang2012 <wangyang1712@gmail.com>
2017-11-24 00:15:59 +0100

    make general menu

--- include/js/common.js
+++ include/js/common.js
@@ -13,3 +13,71 @@ function formatDatetime(value) {
     values = value.split("T");
     return values[0];
 }
+
+webix.ready(function () {
+
+    webix.ui({
+        container: "menu",
+        view: "menu",
+        autowidth: true,
+        data: [
+            {id: "1", value: "TEXTMA"},
+            {id: "2", value: "Accueil"},
+            {
+                id: "3",
+                value: "Client",
+                submenu: ["Clients", "Fournisseurs", "Familles clients", "Clients principaux", "Contacts", "Location longue durée"]
+            },
+            {
+                id: "4", value: "Articles", width: 500,
+                submenu: ["Facebook", "Google+", "Twitter"]
+            },
+            {
+                id: "5", value: "Vente",
+                submenu: ["Commandes", "Google+ Site", "Twitter Site"]
+            },
+            {
+                id: "6", value: "Facturation",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            },
+            {
+                id: "7", value: "Préparation",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            },
+            {
+                id: "8", value: "Confection",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            },
+            {
+                id: "9", value: "Logistique",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            },
+            {
+                id: "10", value: "Production",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            },
+            {
+                id: "11", value: "Stock",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            },
+            {
+                id: "12", value: "Statistiques",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            },
+            {
+                id: "13", value: "Administration",
+                submenu: ["Facebook Site - Social Network", "Google+", "Twitter"]
+            }
+        ],
+        on: {
+            onMenuItemClick: function (id) {
+                webix.message("Click: " + this.getMenuItem(id).value);
+            }
+        },
+        type: {
+            subsign: false,
+            height: 50
+        }
+    });
+
+});
